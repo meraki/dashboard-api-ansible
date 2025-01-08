ae0eb90ae3dcab33d5469639703d30c59f23ff22
Francisco Muñoz <fmunoz@cloverhound.com>
2025-01-08 03:45:27 -0600

    SANITY FIXES

--- plugins/modules/meraki_mr_rf_profile.py
+++ plugins/modules/meraki_mr_rf_profile.py
@@ -189,10 +189,10 @@ EXAMPLES = r'''
     state: present
     name: Test Profile
     band_selection_type: ap
-    client_balancing_enabled: true
+    client_balancing_enabled: True
     ap_band_settings:
-      mode: dual
-      band_steering_enabled: true
+        mode: dual
+        band_steering_enabled: true
     five_ghz_settings:
       max_power: 10
       min_bitrate: 12
@@ -238,10 +238,10 @@ EXAMPLES = r'''
     state: present
     profile_id: 12345
     band_selection_type: ap
-    client_balancing_enabled: true
+    client_balancing_enabled: True
     ap_band_settings:
-      mode: dual
-      band_steering_enabled: true
+        mode: dual
+        band_steering_enabled: true
     five_ghz_settings:
       max_power: 10
       min_bitrate: 12
@@ -278,49 +278,49 @@ data:
     type: complex
     contains:
         id:
-          description:
+            description:
             - Unique identifier of existing RF profile.
-          type: str
-          returned: success
-          sample: 12345
+            type: str
+            returned: success
+            sample: 12345
         band_selection_type:
-          description:
+            description:
             - Sets whether band selection is assigned per access point or SSID.
             - This param is required on creation.
-          type: str
-          returned: success
-          sample: ap
-        min_bitrate_type:
-          description:
-            - Type of minimum bitrate.
-          type: str
-          returned: success
-          sample: ssid
-        name:
-          description:
-          - The unique name of the new profile.
-          - This param is required on creation.
-          type: str
-          returned: success
-          sample: Guest RF profile
-        client_balancing_enabled:
-          description:
-            - Steers client to best available access point.
-          type: bool
-          returned: success
-          sample: true
-        ap_band_settings:
-          description:
-            - Settings that will be enabled if selectionType is set to 'ap'.
-          type: complex
-          returned: success
-          contains:
-            mode:
-              description:
-                - Sets which RF band the AP will support.
             type: str
-                 returned: success
-                 sample: dual
+            returned: success
+            sample: ap
+        min_bitrate_type:
+            description:
+            - Type of minimum bitrate.
+            type: str
+            returned: success
+            sample: ssid
+        name:
+            description:
+            - The unique name of the new profile.
+            - This param is required on creation.
+            type: str
+            returned: success
+            sample: Guest RF profile
+        client_balancing_enabled:
+            description:
+            - Steers client to best available access point.
+            type: bool
+            returned: success
+            sample: true
+        ap_band_settings:
+            description:
+            - Settings that will be enabled if selectionType is set to 'ap'.
+            type: complex
+            returned: success
+            contains:
+                mode:
+                    description:
+                    - Sets which RF band the AP will support.
+                    type: str
+                    returned: success
+                    sample: dual
                 band_steering_enabled:
                     description:
                     - Steers client to most open band.
@@ -329,7 +329,7 @@ data:
                     sample: true
         five_ghz_settings:
             description:
-              - Settings related to 5Ghz band.
+            - Settings related to 5Ghz band.
             type: complex
             returned: success
             contains:
@@ -414,8 +414,9 @@ data:
                     sample: 6
 '''
 
-from ansible_collections.cisco.meraki.plugins.module_utils.network.meraki.meraki import MerakiModule, meraki_argument_spec
 from ansible.module_utils.basic import AnsibleModule, json
+from ansible_collections.cisco.meraki.plugins.module_utils.network.meraki.meraki import MerakiModule, meraki_argument_spec
+
 
 def get_profile(meraki, profiles, name):
     for profile in profiles:
@@ -480,12 +481,10 @@ def main():
                          )
 
     five_arg_spec = dict(max_power=dict(type='int'),
-                         min_bitrate=dict(type='int', choices=[
-                                          6, 9, 12, 18, 24, 36, 48, 54]),
+                         min_bitrate=dict(type='int', choices=[6, 9, 12, 18, 24, 36, 48, 54]),
                          min_power=dict(type='int'),
                          rxsop=dict(type='int'),
-                         channel_width=dict(type='str', choices=[
-                                            'auto', '20', '40', '80']),
+                         channel_width=dict(type='str', choices=['auto', '20', '40', '80']),
                          valid_auto_channels=dict(type='list', elements='int', choices=[36,
                                                                                         40,
                                                                                         44,
@@ -529,8 +528,7 @@ def main():
                         min_power=dict(type='int'),
                         rxsop=dict(type='int'),
                         ax_enabled=dict(type='bool'),
-                        valid_auto_channels=dict(
-                            type='list', elements='int', choices=[1, 6, 11]),
+                        valid_auto_channels=dict(type='list', elements='int', choices=[1, 6, 11]),
                         )
 
     argument_spec = meraki_argument_spec()
@@ -540,18 +538,13 @@ def main():
                          net_name=dict(type='str'),
                          net_id=dict(type='str'),
                          profile_id=dict(type='str', aliases=['id']),
-                         band_selection_type=dict(
-                             type='str', choices=['ssid', 'ap']),
-                         min_bitrate_type=dict(
-                             type='str', choices=['band', 'ssid']),
+                         band_selection_type=dict(type='str', choices=['ssid', 'ap']),
+                         min_bitrate_type=dict(type='str', choices=['band', 'ssid']),
                          name=dict(type='str'),
                          client_balancing_enabled=dict(type='bool'),
-                         ap_band_settings=dict(
-                             type='dict', options=band_arg_spec),
-                         five_ghz_settings=dict(
-                             type='dict', options=five_arg_spec),
-                         two_four_ghz_settings=dict(
-                             type='dict', options=two_arg_spec),
+                         ap_band_settings=dict(type='dict', options=band_arg_spec),
+                         five_ghz_settings=dict(type='dict', options=five_arg_spec),
+                         two_four_ghz_settings=dict(type='dict', options=two_arg_spec),
                          )
 
     # the AnsibleModule object will be our abstraction working with Ansible
@@ -565,15 +558,11 @@ def main():
 
     meraki.params['follow_redirects'] = 'all'
 
-    query_all_urls = {
-        'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles'}
-    query_urls = {
-        'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles/{profile_id}'}
+    query_all_urls = {'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles'}
+    query_urls = {'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles/{profile_id}'}
     create_urls = {'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles'}
-    update_urls = {
-        'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles/{profile_id}'}
-    delete_urls = {
-        'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles/{profile_id}'}
+    update_urls = {'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles/{profile_id}'}
+    delete_urls = {'mr_rf_profile': '/networks/{net_id}/wireless/rfProfiles/{profile_id}'}
 
     meraki.url_catalog['get_all'].update(query_all_urls)
     meraki.url_catalog['get_one'].update(query_urls)
@@ -584,29 +573,23 @@ def main():
     if meraki.params['five_ghz_settings'] is not None:
         if meraki.params['five_ghz_settings']['max_power'] is not None:
             if meraki.params['five_ghz_settings']['max_power'] < 8 or meraki.params['five_ghz_settings']['max_power'] > 30:
-                meraki.fail_json(
-                    msg="5ghz max power must be between 8 and 30.")
+                meraki.fail_json(msg="5ghz max power must be between 8 and 30.")
         if meraki.params['five_ghz_settings']['min_power'] is not None:
             if meraki.params['five_ghz_settings']['min_power'] < 8 or meraki.params['five_ghz_settings']['min_power'] > 30:
-                meraki.fail_json(
-                    msg="5ghz min power must be between 8 and 30.")
+                meraki.fail_json(msg="5ghz min power must be between 8 and 30.")
         if meraki.params['five_ghz_settings']['rxsop'] is not None:
             if meraki.params['five_ghz_settings']['rxsop'] < -95 or meraki.params['five_ghz_settings']['rxsop'] > -65:
-                meraki.fail_json(
-                    msg="5ghz min power must be between 8 and 30.")
+                meraki.fail_json(msg="5ghz min power must be between 8 and 30.")
     if meraki.params['two_four_ghz_settings'] is not None:
         if meraki.params['two_four_ghz_settings']['max_power'] is not None:
             if meraki.params['two_four_ghz_settings']['max_power'] < 5 or meraki.params['two_four_ghz_settings']['max_power'] > 30:
-                meraki.fail_json(
-                    msg="5ghz max power must be between 5 and 30.")
+                meraki.fail_json(msg="5ghz max power must be between 5 and 30.")
         if meraki.params['two_four_ghz_settings']['min_power'] is not None:
             if meraki.params['two_four_ghz_settings']['min_power'] < 5 or meraki.params['two_four_ghz_settings']['min_power'] > 30:
-                meraki.fail_json(
-                    msg="5ghz min power must be between 5 and 30.")
+                meraki.fail_json(msg="5ghz min power must be between 5 and 30.")
         if meraki.params['two_four_ghz_settings']['rxsop'] is not None:
             if meraki.params['two_four_ghz_settings']['rxsop'] < -95 or meraki.params['two_four_ghz_settings']['rxsop'] > -65:
-                meraki.fail_json(
-                    msg="5ghz min power must be between 8 and 30.")
+                meraki.fail_json(msg="5ghz min power must be between 8 and 30.")
 
     org_id = meraki.params['org_id']
     net_id = meraki.params['net_id']
@@ -617,19 +600,16 @@ def main():
         org_id = meraki.get_org_id(meraki.params['org_name'])
     if net_id is None:
         nets = meraki.get_nets(org_id=org_id)
-        net_id = meraki.get_net_id(
-            org_id, meraki.params['net_name'], data=nets)
+        net_id = meraki.get_net_id(org_id, meraki.params['net_name'], data=nets)
     if profile_id is None:
         path = meraki.construct_path('get_all', net_id=net_id)
         profiles = meraki.request(path, method='GET')
         # profile = get_profile(meraki, profiles, meraki.params['name'])
-        profile_id = next(
-            (profile['id'] for profile in profiles if profile['name'] == meraki.params['name']), None)
+        profile_id = next((profile['id'] for profile in profiles if profile['name'] == meraki.params['name']), None)
 
     if meraki.params['state'] == 'query':
         if profile_id is not None:
-            path = meraki.construct_path('get_one', net_id=net_id, custom={
-                                         'profile_id': profile_id})
+            path = meraki.construct_path('get_one', net_id=net_id, custom={'profile_id': profile_id})
             result = meraki.request(path, method='GET')
             meraki.result['data'] = result
             meraki.exit_json(**meraki.result)
@@ -646,24 +626,20 @@ def main():
                 meraki.result['changed'] = True
                 meraki.exit_json(**meraki.result)
             path = meraki.construct_path('create', net_id=net_id)
-            response = meraki.request(
-                path, method='POST', payload=json.dumps(payload))
+            response = meraki.request(path, method='POST', payload=json.dumps(payload))
             meraki.result['data'] = response
             meraki.result['changed'] = True
             meraki.exit_json(**meraki.result)
         else:
-            path = meraki.construct_path('get_one', net_id=net_id, custom={
-                                         'profile_id': profile_id})
+            path = meraki.construct_path('get_one', net_id=net_id, custom={'profile_id': profile_id})
             original = meraki.request(path, method='GET')
             if meraki.is_update_required(original, payload) is True:
                 if meraki.check_mode is True:
                     meraki.result['data'] = payload
                     meraki.result['changed'] = True
                     meraki.exit_json(**meraki.result)
-                path = meraki.construct_path('update', net_id=net_id, custom={
-                                             'profile_id': profile_id})
-                response = meraki.request(
-                    path, method='PUT', payload=json.dumps(payload))
+                path = meraki.construct_path('update', net_id=net_id, custom={'profile_id': profile_id})
+                response = meraki.request(path, method='PUT', payload=json.dumps(payload))
                 meraki.result['data'] = response
                 meraki.result['changed'] = True
                 meraki.exit_json(**meraki.result)
@@ -675,8 +651,7 @@ def main():
             meraki.result['data'] = {}
             meraki.result['changed'] = True
             meraki.exit_json(**meraki.result)
-        path = meraki.construct_path('delete', net_id=net_id, custom={
-                                     'profile_id': profile_id})
+        path = meraki.construct_path('delete', net_id=net_id, custom={'profile_id': profile_id})
         response = meraki.request(path, method='DELETE')
         meraki.result['data'] = {}
         meraki.result['changed'] = True
