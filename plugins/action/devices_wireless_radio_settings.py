6de3f62ffb02f704c94d73b6594c8b8810ad2807
bvargasre <bvargas@cloverhound.com>
2025-04-16 15:22:44 -0600

    Refactor code formatting and improve readability in devices_wireless_radio_settings.py Changed meraki_compare_equality2 to meraki_compare_equality

--- plugins/action/devices_wireless_radio_settings.py
+++ plugins/action/devices_wireless_radio_settings.py
@@ -5,9 +5,11 @@
 # GNU General Public License v3.0+ (see LICENSE or
 # https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 from ansible.plugins.action import ActionBase
+
 try:
     from ansible_collections.ansible.utils.plugins.module_utils.common.argspec_validate import (
         AnsibleArgSpecValidator,
@@ -20,7 +22,7 @@ from ansible.errors import AnsibleActionFail
 from ansible_collections.cisco.meraki.plugins.plugin_utils.meraki import (
     MERAKI,
     meraki_argument_spec,
-    meraki_compare_equality2,
+    meraki_compare_equality,
     get_dict_result,
 )
 from ansible_collections.cisco.meraki.plugins.plugin_utils.exceptions import (
@@ -30,13 +32,15 @@ from ansible_collections.cisco.meraki.plugins.plugin_utils.exceptions import (
 # Get common arguments specification
 argument_spec = meraki_argument_spec()
 # Add arguments specific for this module
-argument_spec.update(dict(
-    state=dict(type="str", default="present", choices=["present"]),
-    fiveGhzSettings=dict(type="dict"),
-    rfProfileId=dict(type="str"),
-    twoFourGhzSettings=dict(type="dict"),
-    serial=dict(type="str"),
-))
+argument_spec.update(
+    dict(
+        state=dict(type="str", default="present", choices=["present"]),
+        fiveGhzSettings=dict(type="dict"),
+        rfProfileId=dict(type="str"),
+        twoFourGhzSettings=dict(type="dict"),
+        serial=dict(type="str"),
+    )
+)
 
 required_if = [
     ("state", "present", ["serial"], True),
@@ -58,24 +62,43 @@ class DevicesWirelessRadioSettings(object):
 
     def get_all_params(self, name=None, id=None):
         new_object_params = {}
-        if self.new_object.get('serial') is not None or self.new_object.get('serial') is not None:
-            new_object_params['serial'] = self.new_object.get('serial')
+        if (
+            self.new_object.get("serial") is not None
+            or self.new_object.get("serial") is not None
+        ):
+            new_object_params["serial"] = self.new_object.get("serial")
         return new_object_params
 
     def update_all_params(self):
         new_object_params = {}
-        if self.new_object.get('fiveGhzSettings') is not None or self.new_object.get('five_ghz_settings') is not None:
-            new_object_params['fiveGhzSettings'] = self.new_object.get('fiveGhzSettings') or \
-                self.new_object.get('five_ghz_settings')
-        if self.new_object.get('rfProfileId') is not None or self.new_object.get('rf_profile_id') is not None:
-            new_object_params['rfProfileId'] = self.new_object.get('rfProfileId') or \
-                self.new_object.get('rf_profile_id')
-        if self.new_object.get('twoFourGhzSettings') is not None or self.new_object.get('two_four_ghz_settings') is not None:
-            new_object_params['twoFourGhzSettings'] = self.new_object.get('twoFourGhzSettings') or \
-                self.new_object.get('two_four_ghz_settings')
-        if self.new_object.get('serial') is not None or self.new_object.get('serial') is not None:
-            new_object_params['serial'] = self.new_object.get('serial') or \
-                self.new_object.get('serial')
+        if (
+            self.new_object.get("fiveGhzSettings") is not None
+            or self.new_object.get("five_ghz_settings") is not None
+        ):
+            new_object_params["fiveGhzSettings"] = self.new_object.get(
+                "fiveGhzSettings"
+            ) or self.new_object.get("five_ghz_settings")
+        if (
+            self.new_object.get("rfProfileId") is not None
+            or self.new_object.get("rf_profile_id") is not None
+        ):
+            new_object_params["rfProfileId"] = self.new_object.get(
+                "rfProfileId"
+            ) or self.new_object.get("rf_profile_id")
+        if (
+            self.new_object.get("twoFourGhzSettings") is not None
+            or self.new_object.get("two_four_ghz_settings") is not None
+        ):
+            new_object_params["twoFourGhzSettings"] = self.new_object.get(
+                "twoFourGhzSettings"
+            ) or self.new_object.get("two_four_ghz_settings")
+        if (
+            self.new_object.get("serial") is not None
+            or self.new_object.get("serial") is not None
+        ):
+            new_object_params["serial"] = self.new_object.get(
+                "serial"
+            ) or self.new_object.get("serial")
         return new_object_params
 
     def get_object_by_name(self, name):
@@ -88,9 +111,9 @@ class DevicesWirelessRadioSettings(object):
                 params=self.get_all_params(name=name),
             )
             if isinstance(items, dict):
-                if 'response' in items:
-                    items = items.get('response')
-            result = get_dict_result(items, 'name', name)
+                if "response" in items:
+                    items = items.get("response")
+            result = get_dict_result(items, "name", name)
             if result is None:
                 result = items
         except Exception as e:
@@ -119,7 +142,8 @@ class DevicesWirelessRadioSettings(object):
             _id = prev_obj.get("id")
             if id_exists and name_exists and o_id != _id:
                 raise InconsistentParameters(
-                    "The 'id' and 'name' params don't refer to the same object")
+                    "The 'id' and 'name' params don't refer to the same object"
+                )
             if _id:
                 self.new_object.update(dict(id=_id))
         it_exists = prev_obj is not None and isinstance(prev_obj, dict)
@@ -136,9 +160,12 @@ class DevicesWirelessRadioSettings(object):
         ]
         # Method 1. Params present in request (Ansible) obj are the same as the current (ISE) params
         # If any does not have eq params, it requires update
-        return any(not meraki_compare_equality2(current_obj.get(meraki_param),
-                                                requested_obj.get(ansible_param))
-                   for (meraki_param, ansible_param) in obj_params)
+        return any(
+            not meraki_compare_equality(
+                current_obj.get(meraki_param), requested_obj.get(ansible_param)
+            )
+            for (meraki_param, ansible_param) in obj_params
+        )
 
     def update(self):
         id = self.new_object.get("id")
@@ -157,7 +184,8 @@ class ActionModule(ActionBase):
     def __init__(self, *args, **kwargs):
         if not ANSIBLE_UTILS_IS_INSTALLED:
             raise AnsibleActionFail(
-                "ansible.utils is not installed. Execute 'ansible-galaxy collection install ansible.utils'")
+                "ansible.utils is not installed. Execute 'ansible-galaxy collection install ansible.utils'"
+            )
         super(ActionModule, self).__init__(*args, **kwargs)
         self._supports_async = False
         self._supports_check_mode = False
@@ -203,8 +231,7 @@ class ActionModule(ActionBase):
                     response = prev_obj
                     meraki.object_already_present()
             else:
-                meraki.fail_json(
-                    "Object does not exists, plugin only has update")
+                meraki.fail_json("Object does not exists, plugin only has update")
 
         self._result.update(dict(meraki_response=response))
         self._result.update(meraki.exit_json())
