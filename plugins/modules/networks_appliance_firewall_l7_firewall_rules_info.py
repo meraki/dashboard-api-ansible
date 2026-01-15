399648dded1de688ac254e1b295a0c7980851482
Francisco Muñoz M <fmunoz@cloverhound.com>
2026-01-15 13:59:32 -0600

    Add new modules for managing and retrieving information on various network and organization resources, including adaptive policy ACLs, groups, VPN peers, camera custom analytics artifacts, config templates, and login security. Enhance documentation for clarity and consistency across modules.

--- plugins/modules/networks_appliance_firewall_l7_firewall_rules_info.py
+++ plugins/modules/networks_appliance_firewall_l7_firewall_rules_info.py
@@ -5,7 +5,7 @@
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 DOCUMENTATION = r"""
-module: networks_appliance_firewall_l7FirewallRules_info
+module: networks_appliance_firewall_l7_firewall_rules_info
 short_description: Information module for networks _appliance _firewall l7firewallrules
 description:
   - Information module for Networks Appliance Firewall L7Firewallrules Info.
